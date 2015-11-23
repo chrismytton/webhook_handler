e57d3edc1e1fe4389adca3d5bb3149f4e2fe088b
Chris Mytton <chrismytton@gmail.com>
2015-11-23 21:30:03 +0000

    Implementation for WebhookHandler

--- lib/webhook_handler.rb
+++ lib/webhook_handler.rb
@@ -1,5 +1,25 @@
 require 'webhook_handler/version'
+require 'sidekiq'
+require 'sinatra'
 
 module WebhookHandler
-  # Your code goes here...
+  def self.included(klass)
+    klass.extend self
+    klass.include Sidekiq::Worker
+    klass.extend Sinatra::Delegator
+    klass.instance_eval do
+      get '/' do
+        'Send a POST request to this URL to trigger the webhook'
+      end
+
+      post '/' do
+        klass.perform_async
+        'ok'
+      end
+    end
+  end
+
+  def call(*args)
+    Sinatra::Application.call(*args)
+  end
 end
--- test/webhook_handler_test.rb
+++ test/webhook_handler_test.rb
@@ -1,11 +1,44 @@
 require 'test_helper'
+require 'rack/test'
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
+
+class MyApp
+  include WebhookHandler
+
+  def perform
+  end
+end
 
 class WebhookHandlerTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    MyApp
+  end
+
+  def after_teardown
+    Sidekiq::Worker.clear_all
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::WebhookHandler::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_is_a_sidekiq_worker
+    assert_equal 0, app.jobs.size
+    app.perform_async
+    assert_equal 1, app.jobs.size
+  end
+
+  def test_homepage
+    get '/'
+    assert_equal 'Send a POST request to this URL to trigger the webhook', last_response.body
+  end
+
+  def test_triggering_webhook
+    assert_equal 0, app.jobs.size
+    post '/'
+    assert_equal 1, app.jobs.size
   end
 end
